b8727d413baef41ab4b42de4ea088aea8cff27f8
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2024-04-17 23:02:02 +0400

    Scrollbar plugin

---
+++ lua/plugins/scrollbar.lua
@@ -0,0 +1,11 @@
+return {
+    "petertriho/nvim-scrollbar",
+    event = "LazyFile",
+    dependencies = { "lewis6991/gitsigns.nvim" },
+    opts = {},
+    config = function(_, opts)
+        require("scrollbar").setup(opts)
+        require("scrollbar.handlers.gitsigns").setup()
+        require("scrollbar.handlers.search").setup()
+    end,
+}
