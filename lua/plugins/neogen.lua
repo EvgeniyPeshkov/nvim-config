747bed544f1ba8357d70a959832a95ea5feb6a92
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2024-04-22 17:01:04 +0400

    Doc generation plugin

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,8 @@
+return {
+    "danymat/neogen",
+    config = true,
+    cmd = { "Neogen" },
+    keys = {
+        { "<leader>ck", "<cmd>Neogen<cr>", mode = "n", desc = "Generate Doc" },
+    },
+}
