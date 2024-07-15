c4af8aab247f191ac6702b7829a917a339c02179
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2024-07-15 19:05:27 +0400

    Use neogen packaged with a distro

--- lua/plugins/neogen.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "danymat/neogen",
-    config = true,
-    cmd = { "Neogen" },
-    keys = {
-        { "<leader>ck", "<cmd>Neogen<cr>", mode = "n", desc = "Generate Doc" },
-    },
-}
