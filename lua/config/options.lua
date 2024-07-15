e3815673da3fe3a97a3887601fcc4d883168eaad
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2024-07-15 17:42:27 +0400

    Settings for windows and neovide client

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,15 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.g.autoformat = false
 vim.g.fixeol = true
+if vim.fn.has("win32") then
+    vim.o.shell = "pwsh.exe"
+end
+if (vim.g.neovide) then
+    vim.o.guifont = "CaskaydiaMono Nerd Font Mono:h10:b"
+    vim.g.neovide_cursor_animation_length = 0.01
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_scroll_animation_length = 0.05
+    vim.g.neovide_floating_shadow = false
+    vim.g.neovide_floating_blur_amount_x = 0.2
+    vim.g.neovide_floating_blur_amount_y = 0.2
+end
