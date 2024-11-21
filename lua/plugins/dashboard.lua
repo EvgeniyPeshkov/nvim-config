50c701ae2ca27fdaf8dc33f8d39fc95f2e7b53f7
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2024-11-21 17:53:18 +0400

    Update dashboard opts for snacks.nvim

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,10 +1,22 @@
 return {
-    "nvimdev/dashboard-nvim",
-    opts = function (_, opts)
-        for _, v in ipairs(opts.config.center) do
-            if string.find(v.desc, "Restore Session") then
-                v.action = 'lua require("persistence").load({last=true})'
-            end
-        end
-    end
+    "folke/snacks.nvim",
+    opts = {
+        dashboard = {
+            preset = {
+                -- stylua: ignore
+                ---@type snacks.dashboard.Item[]
+                keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                { icon = " ", key = "s", desc = "Restore Session", action = ':lua require("persistence").load({last=true})' },
+                { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+                { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+        },
+    },
 }
