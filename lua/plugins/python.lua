9a47ebfb33e2a15643d4913ec76573d08eb22ebe
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2025-01-06 16:35:36 +0300

    Rename git config, update python setup

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,5 +1,31 @@
 return {
     {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            local path = require("mason-registry").get_package("debugpy"):get_install_path()
+            local exe = path .. "/venv/bin/python"
+            if vim.fn.has("win32") == 1 then
+                exe = path .. "/venv/Scripts/pythonw"
+            end
+            local dap = require("dap-python")
+            dap.resolve_python = function()
+                return vim.fn.exepath("python")
+            end
+            dap.setup(exe)
+        end,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        opts = {
+            handlers = {
+                python = function(config)
+                    config.configurations = {}
+                    config.adapters = {}
+                end
+            }
+        }
+    },
+    {
         "linux-cultist/venv-selector.nvim",
         -- opts = { dap_enabled = false },
         -- stylua: ignore
