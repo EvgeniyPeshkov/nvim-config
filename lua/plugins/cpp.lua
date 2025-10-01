5f1242043cfd556530fe7c91b002dec77a018592
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2025-10-01 18:10:10 +0400

    Configure cmake tools

---
+++ lua/plugins/cpp.lua
@@ -0,0 +1,14 @@
+return {
+    "cmake-tools.nvim",
+    opts = {
+        cmake_runner = {
+            name = "quickfix",
+            opts = {
+                show = "always",
+                focus = true,
+                start_insert = true,
+                close_on_exit = true
+            },
+        },
+    },
+}
