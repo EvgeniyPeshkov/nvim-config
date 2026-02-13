187e0bd46246a04aa6078fc8b04e66a68ce29199
EvgeniyPeshkov <50870158+EvgeniyPeshkov@users.noreply.github.com>
2026-02-14 02:28:41 +0400

    Autocompletion by fim llm

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,4 +14,12 @@ return {
             },
         },
     },
+    {
+        "ggml-org/llama.vim",
+        init = function()
+            vim.g.llama_config = {
+                enable_at_startup = false,
+            }
+        end,
+    },
 }
